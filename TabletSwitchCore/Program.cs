ab75737b7de9c76403c96590a76637e1da8e64da
cynicalrockstar <18341366+cynicalrockstar@users.noreply.github.com>
2019-11-15 22:37:22 -0700

    Add files via upload

---
+++ TabletSwitchCore/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Timers;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace TabletSwitchCore
+{
+    enum Scaling
+    {
+        Screen300 = 3,
+        Screen250 = 2,
+        Screen225 = 1,
+        ScreenDefault = 0,
+        Screen175 = -1,
+        Screen150 = -2,
+        Screen125 = -3,
+        Screen100 = -4
+    }
+
+    class Program
+    {
+        static System.Timers.Timer timer = new System.Timers.Timer();
+
+        static void Main(string[] args)
+        {
+            timer.Interval = 1000;
+            timer.AutoReset = true;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+
+            //The timer will run on its own thread. Sleep the main one to keep the app resident forever
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        private static void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var state = Win32.GetSystemMetrics(Win32.SM_CONVERTIBLESLATEMODE);
+            if (state == 0)
+            {
+                //Tablet mode
+                //This is 200% on surface devices
+                ChangeDPI((int)Scaling.ScreenDefault);
+            }
+            else
+            {
+                //Desktop mode
+                ChangeDPI((int)Scaling.Screen150);
+            }
+        }
+
+        private static void ChangeDPI(int dpi)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("Control Panel", true);
+
+            key = key.OpenSubKey("Desktop", true);
+            key = key.OpenSubKey("PerMonitorSettings", true);
+
+            //TODO: Get your own monitor ID from the registry and put it here
+            key = key.OpenSubKey("LGD0555600224_00_07E2_41^6DF395BF1D440664DC6515C277A800D6", true);
+
+            var current = Convert.ToInt32(key.GetValue("DpiValue"));
+            if (current == dpi)
+                return;
+
+            key.SetValue("DpiValue", dpi);
+
+            //jog the resolution so Windows notices the dpi change
+            //might be a better way, couldn't find one
+            SetResolution(1920, 1280);
+            SetResolution(2880, 1920);
+        }
+
+        private static void SetResolution(int w, int h)
+        {
+            Win32.DEVMODE dm = new Win32.DEVMODE();
+
+            dm.dmSize = (short)Marshal.SizeOf(typeof(Win32.DEVMODE));
+
+            dm.dmPelsWidth = w;
+            dm.dmPelsHeight = h;
+
+            dm.dmFields = Win32.DEVMODE.DM_PELSWIDTH | Win32.DEVMODE.DM_PELSHEIGHT;
+
+            Win32.ChangeDisplaySettings(ref dm, 0);
+        }
+    }
+}
---
+++ TabletSwitchCore/Win32.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace TabletSwitchCore
+{
+    internal static class Win32
+    {
+        public static readonly int SM_CONVERTIBLESLATEMODE = 0x2003;
+        public static readonly int SM_TABLETPC = 0x56;
+
+        public enum DMDO
+        {
+            DEFAULT = 0,
+            D90 = 1,
+            D180 = 2,
+            D270 = 3
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public struct DEVMODE
+        {
+            public const int DM_PELSWIDTH = 0x80000;
+            public const int DM_PELSHEIGHT = 0x100000;
+            private const int CCHDEVICENAME = 32;
+            private const int CCHFORMNAME = 32;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHDEVICENAME)]
+            public string dmDeviceName;
+            public short dmSpecVersion;
+            public short dmDriverVersion;
+            public short dmSize;
+            public short dmDriverExtra;
+            public int dmFields;
+
+            public int dmPositionX;
+            public int dmPositionY;
+            public DMDO dmDisplayOrientation;
+            public int dmDisplayFixedOutput;
+
+            public short dmColor;
+            public short dmDuplex;
+            public short dmYResolution;
+            public short dmTTOption;
+            public short dmCollate;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHFORMNAME)]
+            public string dmFormName;
+            public short dmLogPixels;
+            public int dmBitsPerPel;
+            public int dmPelsWidth;
+            public int dmPelsHeight;
+            public int dmDisplayFlags;
+            public int dmDisplayFrequency;
+            public int dmICMMethod;
+            public int dmICMIntent;
+            public int dmMediaType;
+            public int dmDitherType;
+            public int dmReserved1;
+            public int dmReserved2;
+            public int dmPanningWidth;
+            public int dmPanningHeight;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int ChangeDisplaySettings([In] ref DEVMODE lpDevMode, int dwFlags);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto, EntryPoint = "GetSystemMetrics")]
+        public static extern int GetSystemMetrics(int nIndex);
+    }
+}
